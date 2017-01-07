1c386e798a123aeb9166cc4fe4ac3eb4b2734051
aurthurm <peacewise1@gmail.com>
2017-01-07 14:20:30 +0200

    added WestdgardRules

--- Quality Control SPC_modified.R
+++ Quality Control SPC_modified.R
@@ -1,19 +1,20 @@
 # Statistical Process Control
 # Levy-Jennings Chart
 # Author : Aurthur Musendame
-
 rm(list = ls())
+setwd("~/Documents/working/")
+source("./builds/westgard_rules.R")
 
 # set.seed( seed = NULL)
 
 # LJStats
 LjStats <- function(x){
-  cl.sds      <<- -4:4 + mean(x)  # Mean +/- -4:4 Standard Deviations
-  cl.ylim     <<- c(cl.sds[1], cl.sds[9]) # Y Limit : 4 standard devitions
+  cl.sds      <<- -3:3 * sd(x) + mean(x)  # Mean +/- -4:4 Standard Deviations
+  cl.ylim     <<- c(-4 * sd(x) + mean(x), 4 *sd(x) + mean(x)) # Y Limit : 4 standard devitions
   cl.sd.names <- c("-3 SD", "-2 SD", "-1 SD", "Mean", "1 SD", "2 SD", "3 SD")  # Standard Deviation Labels
-  cl.base     <<- data.frame(cl.sd.names, cl.sds[2]:cl.sds[8], stringsAsFactors = FALSE)
+  cl.base     <<- data.frame(cl.sd.names, cl.sds, stringsAsFactors = FALSE)
   return(c(Obs = length(x),
-           Mean = cl.sds[5],
+           Mean = cl.sds[4],
            SD = sd(x)))
 }
 
@@ -26,43 +27,51 @@ LjObs <- function(x){
 
 # Plot the SPC Chart
 LjChart <- function(x, title = main.title){
-  LjStats(x)
+  statz = LjStats(x)
   days  <- 1:LjObs(x)
+  controls <- WestgardRules(x)
   # Set the text and margin sizes
   par(cex.axis = 0.8, mai = c(1, 0.7, 0.7, 1), fig = c(0, 0.95, 0, 1))
   # Plotting the SPC
-  plot(days, x,
+  plot(days, controls$x,
        xaxt = "n",
        yaxt = "n",
        pch  = 20, # use also 1, 4, 18 etc
-       lty  = 3,
-       ylim = cl.ylim,  # c(cl.base[,2][7],cl.base[,2][1])
+       lty  = 1,
+       ylim = cl.ylim,
        type = "b",
-       col  = "green",
+       col  = controls$color,
        ylab = "",
        xlab = "Control Run Days",
        main = title
   )
   #  Draw the Standard Deviation Lines
   abline(h   = c(cl.base[, 2]), 
-         col = c("red", "orange", "yellow", "green", "yellow", "orange", "red"))
+         col = c("red", "orange", "yellow", "green", "yellow", "orange", "red"), lty = 5)
   axis(1, at = days, labels = days, las = 2)
   # Left Labels 
   axis(2, at = cl.base[, 2], labels = cl.base[, 1], las = 2) 
   # Right Labels - Numerical
   axis(4, at = cl.base[,2], labels = round(cl.base[,2], digits = 3), las = 2)
+  # adding rule text to the graph
+  text(controls$x, controls$rule, cex = 0.7, pos = 3)
+  # adding LjStats()
+  mtext("Obs: " , side = 3, adj = 0)
+  mtext( statz[1] , side = 3, adj = 0.085)
+  mtext("Mean: " , side = 3, adj = 0.45)
+  mtext( round(statz[2], 3), side = 3, adj = 0.572)
+  mtext("SD: " , side = 3, adj = 0.9)
+  mtext( round(statz[3], 3) , side = 3, adj = 0.997)
   ## Add a BoxPlot to the plot
-  #par(fig = c(0.675, 1, 0, 1), new = TRUE)
-  #boxplot(cl.base[, 2], axes = FALSE)
+  # par(fig = c(0.675, 1, 0, 1), new = TRUE)
+  # boxplot(cl.base[, 2], axes = FALSE)
 }
 
-normal.control <- rnorm(1:30,5,1.3)
-main.title <- "Levey Jennings Chart \n Normal Control "
-#normal control
-LjChart(normal.control)
-normal.control
-LjStats(normal.control)
 
+main.title <- "Levey Jennings Chart  Normal Control \n"
+normal.control <- rnorm(1:30,52,4.3)
+ LjChart(normal.control)
+ LjStats(normal.control)
 
 
 
---
+++ westgard_rules.R
@@ -0,0 +1,75 @@
+
+WestgardRules <- function(x){
+    x          <- as.data.frame(x)
+    x$color    = "green"
+    x$rule     = " "
+    x_mean     = mean(x$x)
+    x_sd       = sd(x$x)
+    x_sds      = -3:3 * x_sd + x_mean
+    minus_3_sd = x_sds[1]
+    minus_2_sd = x_sds[2]
+    minus_1_sd = x_sds[3]
+    plus_1_sd  = x_sds[5]
+    plus_2_sd  = x_sds[6]
+    plus_3_sd  = x_sds[7]
+    x_len      = 1:length(x$x)
+    
+    for (i in x_len) {
+	    # 1_3s Rule:
+	    # 1 control result outside 3sd
+        if(x$x[i] > plus_3_sd | x$x[i] < minus_3_sd){
+            x$color[i] = "red"
+            x$rule[i]  = "1_3s" 
+        }
+        # 1_2s Rule : Warning rule
+    	# one of two control results fall outside +- 2SD
+    	if(x$x[i] <= plus_3_sd & x$x[i] > plus_2_sd |
+    		x$x[i] >= minus_3_sd & x$x[i] < minus_2_sd) {
+    		x$color[i] = "orange"
+    		x$rule[i]  = "1_2s"
+    		# 2_2s Rule:
+		    # two control results fall outside 2sd in the same direction
+		    # or both controls in the same run exceeded 2sd
+    		if(i > 1) {
+    		    if(x$x[i-1] <= plus_3_sd & x$x[i-1] > plus_2_sd |
+    		       x$x[i-1] >= minus_3_sd & x$x[i-1] < minus_2_sd) {
+        		        x$color[i] = "red"
+        		        x$rule[i]  = "2_2s"
+    		    }
+    		}
+    	}
+	    # R_4s  Rule:
+	    # Difference between two runs equals or exceeds 4sd	    
+    	if(i > 1){
+    		if(abs(x$x[i] - x$x[i -1]) >= x_sd * 4){
+    			x$color[i] = "red"
+    			x$rule[i]  = "R_4s"
+    		}
+    	}
+    	# 4_1s:
+    	# 4 consecutive controls outside 1sd
+    	if(i > 3){
+    		if(x$x[i] > x_mean & x$x[i] <= plus_1_sd &
+    			x$x[i - 1] > x_mean & x$x[i - 1] <= plus_1_sd &
+    			x$x[i - 2] > x_mean & x$x[i - 2] <= plus_1_sd &
+    			x$x[i - 3] > x_mean & x$x[i - 3] <= plus_1_sd | 
+    			x$x[i] < x_mean & x$x[i] >= plus_1_sd &
+    			x$x[i - 1] < x_mean & x$x[i - 1] >= minus_1_sd &
+    			x$x[i - 2] < x_mean & x$x[i - 2] >= minus_1_sd &
+    			x$x[i - 3] < x_mean & x$x[i - 3] >= minus_1_sd){
+    			    x$color[i] = "red"
+    				x$rule[i]  = "4_1s"
+                    # A correction to make sure that even if the fifth control lies in the same 
+                    # range it will not be regarded as a 4_1s.
+    				if (x$rule[i-1] == "4_1s"){
+    				    x$color[i] = "green"
+    				    x$rule[i] = " "
+    				}
+    		}
+    	}
+    	# 10_x Rule:
+    	# 10 consecutive control results are on the same sideof the mean
+    	# Not Done. Yu can create it and share with me
+    }
+    return (x)
+}
